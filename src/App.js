b63c99dc23fbbcf2486c6dbec226c0de0fdbc5ef
KumarKhailendra <khailendrakumar417@gmail.com>
2023-12-11 20:47:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [playButton, setPlayButton] = useState(false);
+  const [totalMinutes, setTotalMinutes] = useState(0);
+  const [minutes, setMinutes] = useState("00");
+  const [hours, setHours] = useState("00");
+  const [second, setSecond] = useState("00");
+  const [count, setCount] = useState(0);
+
+  function toHoursAndMinutes(totalMinutes, counter) {
+
+    setHours(padTo2Digits(Math.floor((totalMinutes * 60 - counter) / 60 / 60)));
+    setMinutes(
+      padTo2Digits(Math.floor(((totalMinutes * 60 - counter) / 60) % 60))
+    );
+    setSecond(padTo2Digits(Math.floor((totalMinutes * 60 - counter) % 60)));
+
+    }
+  function padTo2Digits(num) {
+    return num.toString().padStart(2, "0");
+  }
+
+
+  useEffect(() => {
+
+    if (!totalMinutes) {
+      return;
+    }
+
+    if (!playButton) {
+
+      
+      toHoursAndMinutes(totalMinutes, count);
+      return;
+    }
+
+
+    const getCountdownTime = totalMinutes * 60;
+
+    let interval = setTimeout(() => {
+      if (count >= getCountdownTime) {
+        clearInterval(interval);
+
+        return;
+      }
+
+
+  
+
+      toHoursAndMinutes(totalMinutes, count+1);
+
+      setCount((prevValue) => prevValue + 1);
+    }, 1000);
+  }, [totalMinutes, count, playButton]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="counter-container">
+          <div className="minutes-input">
+            <label htmlFor="minutes">Enter Minutes</label>
+            <input
+              type="number"
+              pattern="\d+"
+              name="minutes"
+              onChange={(e) => {
+                setTotalMinutes(e.target.value);
+                setCount(0);
+                setPlayButton(false)
+              }}
+              value={totalMinutes}
+            />
+          </div>
+          <div className="counter-output">
+            <div
+              className="play-button"
+              onClick={() => {
+                setPlayButton(!playButton);
+              }}
+            >
+              {playButton ? (
+                <span className="material-icons">pause</span>
+              ) : (
+                <span className="material-icons">play_arrow</span>
+              )}
+            </div>
+            <div className="counter-timer">
+              {hours + ":" + minutes + ":" + second}
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
